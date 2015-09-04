a3acd9f085f53f59a046847918c8ae10ab62e352
josh <joshualilly@me.com>
2015-09-03 20:28:52 -0400

    Added junit tests

---
+++ src/com/joshua/lilly/dnsdynamic/TestPrimes.java
@@ -0,0 +1,14 @@
+package com.joshua.lilly.dnsdynamic;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestPrimes {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
